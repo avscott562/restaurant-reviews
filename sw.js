1a3405748d797b597c7498a631604287a8295540
aliciascott <avscott@pacbell.net>
2019-03-27 23:12:54 -0700

    fix sw activate

--- sw.js
+++ sw.js
@@ -22,34 +22,38 @@ const cacheFiles = [
 ];
 
 self.addEventListener('install', function(event) {
-  console.log('I am installing');
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(cacheFiles);
     })
   );
 });
-//
-// self.addEventListener('activate', function(event) {
-//   event.waitUntil(
-//     caches.keys().then(function(cacheNames) {
-//       return Promise.all(cacheNames.map(function(thisCacheName) {
-//         if (thisCacheName !== cacheName) {
-//           return caches.delete(thisCacheName);
-//         }
-//       }))
-//     })
-//   );
-// });
-//
-self.addEventListener('fetch', function (event) {
-  console.log('I am fetching');
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(cacheNames.map(function(thisCacheName) {
+        if (thisCacheName !== cacheName) {
+          return caches.delete(thisCacheName);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {  
   event.respondWith(
     caches.match(event.request).then(function(response) {
       if (response) {
         return response;
       }
-      return fetch(event.request);
+      return fetch(event.request).then(function(response) {
+        const clone = response.clone();
+        caches.open(cacheName).then(function(cache) {
+          cache.put(event.request, clone);
+        })
+        return response;
+      })
     })
   );
 });
